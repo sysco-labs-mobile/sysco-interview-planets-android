83df9ac46ec23eebe0c0eae174b75089b6d7e808
Alex Stolzberg <alex.stolzberg@sysco.com>
2020-08-28 15:55:38 -0400

    Fixed dagger dependencies

--- app/src/main/java/com/example/planetslist/di/app/AppModule.kt
+++ app/src/main/java/com/example/planetslist/di/app/AppModule.kt
@@ -2,12 +2,16 @@ package com.example.planetslist.di.app
 
 import android.content.Context
 import com.example.planetslist.PlanetsListApplication
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
+import dagger.Reusable
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
@@ -28,11 +32,20 @@ open class AppModule(private val app: PlanetsListApplication) {
         .build()
 
     @Provides
+    @Reusable
+    internal fun providesMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
     @Singleton
-    fun provideRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(client: OkHttpClient, moshi: Moshi): Retrofit = Retrofit.Builder()
         .baseUrl("")
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .client(client)
         .build()
 
